83a7f318eb82824da91aa4d37abdff14f0c515f9
tsukushibito <tsukushibito@gmail.com>
2019-10-09 15:47:57 +0900

    Add DeckTest

---
+++ DaifugoTest/DeckTest.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using Xunit;
+using Daifugo;
+
+namespace DaifugoTest
+{
+    public class DeckTest
+    {
+        [Fact]
+        public void Pop_DefaultDeck_DecreasedCards()
+        {
+            var deck = new Deck();
+            var cardCount = deck.Cards.Count();
+            deck.Pop();
+            var decreasedCardCount = deck.Cards.Count();
+            var diff = cardCount - decreasedCardCount;
+            Assert.Equal(1, diff);
+        }
+
+        [Fact]
+        public void Pop_EmptyDeck_ReturnNull()
+        {
+            var deck = new Deck();
+            var cardCount = deck.Cards.Count();
+            for (int i = 0; i < 53; ++i) {
+                deck.Pop();
+            }
+
+            var popped = deck.Pop();
+            Assert.False(popped.HasValue);
+        }
+    }
+}
